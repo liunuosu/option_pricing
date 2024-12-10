430eb14a7b84b1d6d4887c0acca4857d04a133ff
Liu Nuo Su <84078936+liunuosu@users.noreply.github.com>
2024-12-10 02:03:27 +0100

    main configuration

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 import pandas as pd 
-from utils import get_config
+from utils import get_config, print_config
+import yaml
+import time
+from datetime import datetime
+
+
 
 def main(config_file):
-    print('wetji')
+
+    # print all information before starting the run
+    print("Configuration Parameters:")
+    print_config(config_file)
+    timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    print('start:', timestamp)
+    start_time = time.time()
+
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+
+    print(f"\nTime taken to run the code: {elapsed_time:.2f} seconds")
+
 
 if __name__ == "__main__":
     config = get_config()
-    print(config['training'])
-
     main(config)
 
     
